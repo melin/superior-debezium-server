ed1ab1bc80e197f165e3362dc32ff86e1d48b057
melin <libinsong1204@gmail.com>
2023-06-08 23:37:02 +0800

    first commit

---
+++ src/main/java/io/github/melin/debezium/server/superiordebeziumserver/SuperiorDebeziumServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.melin.debezium.server.superiordebeziumserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperiorDebeziumServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperiorDebeziumServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/melin/debezium/server/superiordebeziumserver/SuperiorDebeziumServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.melin.debezium.server.superiordebeziumserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperiorDebeziumServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
